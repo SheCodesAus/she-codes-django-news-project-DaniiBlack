0b38ba9895b2210e90314827ebaa25d162829b98
Dani <danisteele1984@gmail.com>
2022-12-06 19:05:18 +1100

    ignore. updated location of some code, nothing

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'news:index'
+LOGOUT_REDIRECT_URL = 'news:index'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,8 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'news:index'
-LOGOUT_REDIRECT_URL = 'news:index'
 
 STATICFILES_DIR = [
     BASE_DIR / 'static',
